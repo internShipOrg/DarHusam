2c7c7603ce94d3e52f58abdb397476f15654ce72
Aliabuhassan2002 <ali.mohammad.abuhassan.se@gmail.com>
2025-05-17 17:26:13 +0300

    get dashboard code

--- serverSide/server.js
+++ serverSide/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const userRoutes = require("./routes/userRoutes");
+
 const newsRoutes = require("./routes/newsRoutes");
 const connectDB = require("./config/db");
 
@@ -33,7 +33,6 @@ cron.schedule(
   { timezone: "Asia/Amman" }
 );
 
-const connectDB = require("./config/db");
 const volunteerRoutes = require("./routes/volunteerRoutes");
 const trainerRoutes = require("./routes/trainerRoutes");
 const traineeRoutes = require("./routes/traineeRoutes");
@@ -44,8 +43,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Increase payload limit to 10MB
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // Enable CORS
 app.use(cors());
@@ -55,7 +54,6 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", userRoutes);
 app.use("/api/news", newsRoutes);
 app.use("/api", volunteerRoutes);
 app.use("/api", trainerRoutes);
