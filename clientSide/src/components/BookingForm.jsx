1e4bcc7aa1db1607f141c692355d755d31161c74
A7madtabaza <ahmadtabaza272@gmail.com>
2025-05-30 17:43:42 +0300

    ahmadtabaza

--- clientSide/src/components/BookingForm.jsx
+++ clientSide/src/components/BookingForm.jsx
@@ -28,7 +28,7 @@ const BookingForm = ({ hall, bookedTimes, selectedDate, onBookingSuccess }) => {
 
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:9000/api/bookings", {
+      const response = await fetch("http://localhost:5000/api/bookings", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
